8559667058df157ecca899c505c0ae4bacbdbb68
thai-hoang-03 <buithaihoang03@gmail.com>
2025-03-15 17:32:49 +0700

    update role photo-er

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import LandingPage from "./pages/LandingPage";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/partials/Navbar";
+import WaitingList from "./components/photograper/WaitingList";
+import AcceptedList from "./components/photograper/AcceptedList";
+import FinishList from "./components/photograper/FinishList";
+import LandingPage from "./pages/LandingPage"
+
+
 
 function App() {
-    const [count, setCount] = useState(0);
-
-    return (
-        <>
-            <LandingPage />
-        </>
-    );
+  return (
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<LandingPage/>} />
+        <Route path="/waiting-list" element={<WaitingList/>} />
+        <Route path="/accepted-list" element={<AcceptedList/>} />
+        <Route path="/finish-list" element={<FinishList/>} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
